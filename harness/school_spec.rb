8d43ba5a8b1b39d14138a40c5a34577129f9f7d5
Erin Pintozzi <epintozzi@turing.edu>
2022-07-06 15:32:40 -0500

    add spec harness and instructor readme

--- harness/school_spec.rb
+++ harness/school_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 1' do
-    xit '1. School Instantiation' do
+    it '1. School Instantiation' do
       expect(School).to respond_to(:new).with(2).argument
       expect(@school1).to be_an_instance_of(School)
 
@@ -35,7 +35,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 2' do
-    xit '2. School #add_student_name' do
+    it '2. School #add_student_name' do
       expect(@school1).to respond_to(:add_student_name).with(1).argument
 
       @school1.add_student_name('Megan')
@@ -45,7 +45,7 @@ RSpec.describe 'School Spec Harness' do
       expect(@school1.student_names).to eq(['Megan', 'Aurora', 'Tim'])
     end
 
-    xit '3. School #end_time' do
+    it '3. School #end_time' do
       expect(@school1).to respond_to(:end_time).with(0).argument
       expect(@school1.end_time).to eq('16:00')
     end

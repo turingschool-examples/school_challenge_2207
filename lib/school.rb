857c91ba2c3d41d424834be89a59333451c57f38
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-07-07 14:03:15 -0400

    Unfinished attempt

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,22 @@
 class School
     attr_accessor :school1, :school2
-    def initialize(school1, school2)
+    def initialize(school1, school2, start_time = [])
         @school1= school1
         @school2=school2
-        @start_time = []
+        @start_time = start_time
 
     end
 
     def start_time (start)
-        @start_time << start
+        @start_time << start#why is start not linking to line 10? 
     end
 
     def hours_in_school_day(HRs)
 
+    end
+
+    def student_names(name)
+        
     end
 
 

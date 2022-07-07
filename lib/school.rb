fb8ad6c3891dd01cccdb6c4ee52d738fda19c29d
Sid Mann <sjmann2@outlook.com>
2022-07-07 12:58:03 -0500

    Attempt to complete end_time method

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,19 @@
 class School
-    attr_reader :start_time, :end_time, :hours_in_school_day, :student_names
+    attr_reader :start_time, :hours_in_school_day, :student_names
     def initialize(start_time, hours_in_school_day)
         @start_time = start_time
         @hours_in_school_day = hours_in_school_day
         @student_names = []
-        @end_time = end_time
     end
 
     def add_student_name(name)
         @student_names << name
     end
 
-    #How to convert integer 16 back to string military time
+    #How to convert float 16.0 back to string military time
     def end_time
-        @start_time.to_i + @hours_in_school_day
+        @start_time.to_f + @hours_in_school_day.to_f
+        
     end
 
     def is_full_time?

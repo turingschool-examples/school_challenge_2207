e5c1468aef5e65daf096592758fb0d25b57f4eb5
Ashley <ashuhleyt@gmail.com>
2022-07-07 11:01:30 -0700

    Added Work

---
+++ lib/runner.rb
@@ -0,0 +1,8 @@
+require './School'
+
+@school1 = School.new('9:00', 7)
+
+@school2 = School.new('10:00', 2)
+
+
+require 'pry'; binding.pry
--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
+class School
+  def initialize(start_time, length, student_names)
+    @start_time = start_time
+    @length = length
+    @student_names = []
+  end
 
+  def start_time
+    @start_time
+  end
+
+  def length
+    @length
+  end
+
+  def student_names
+    @student_names
+  end
+
+end

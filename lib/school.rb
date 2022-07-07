2c15134be1c3ade931b5a8331cf78a121c17d6b1
Sid Mann <sjmann2@outlook.com>
2022-07-07 11:58:53 -0500

    Add iterations 1 and 2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 class School
+    attr_reader :start_time, :end_time, :hours_in_school_day, :student_names
+    def initialize(start_time, hours_in_school_day)
+        @start_time = start_time
+        @hours_in_school_day = hours_in_school_day
+        @student_names = []
+        @end_time = end_time
+    end
+
+    def add_student_name(name)
+        @student_names << name
+    end
+
+    #How to convert integer 16 back to string military time
+    def end_time
+        @start_time.to_i + @hours_in_school_day
+    end
+end

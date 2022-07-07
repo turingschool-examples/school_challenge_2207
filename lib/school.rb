41bf98241f539730d534243a54a7c2ba3c5e2a03
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-07 14:02:40 -0400

    first commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
-
+class School
+   
+   attr_reader :start_time, :hours_in_school_day
+    
+   def initalize(start, hours, name)
+        @start_time = start
+        @hours_in_school_day = hours
+        @student_names = name
+    
+    end
+end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 require './lib/school'
 
 
@@ -11,12 +11,12 @@ end
 
 RSpec.describe 'School Spec Harness' do
   before :each do
-    @school1 = School.new('9:00', 7)
+    @school1 = School.new('9:00', 7)it 
     @school2 = School.new('10:00', 2)
   end
 
   describe 'Iteration 1' do
-    xit '1. School Instantiation' do
+    it '1. School Instantiation' do
       expect(School).to respond_to(:new).with(2).argument
       expect(@school1).to be_an_instance_of(School)
 

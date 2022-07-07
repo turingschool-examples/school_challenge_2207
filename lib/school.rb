36e7d5a9f47e39db4db918eaee1732b0aaf434fb
Kyongnam Kang <kangkna@gmail.com>
2022-07-07 12:55:30 -0400

    Define add student name

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,10 @@ class School
     def initialize(start_time, hours_in_school_day)
         @hours_in_school_day = hours_in_school_day
         @start_time = start_time
-        @student_names = student_names
+        @student_names = []
+    end
+
+    def add_student_name(student)
+        @student_names << student
     end
 end

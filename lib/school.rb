53cae19ec9f2d3a90f48c586d741b51efcef4fc2
Kevin Ta <kvnta@yahoo.com>
2022-07-07 12:03:04 -0600

    Messed up and cloned down turing example. Rookie mistake. Had more commits other then this one

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
 
+  def add_student_name(student)
+    @student_names << student
+  end
+
+  def end_time
+    (@start_time.to_i + hours_in_school_day).to_s + ":00"
+  end
+
+  def is_full_time?
+    if hours_in_school_day >= 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names
+    @student_names.map { |student| student.capitalize }
+  end
+
+  def convert_end_time_to_clock_time
+    (end_time.to_i - 12).to_s + ":00"
+  end
+end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require 'pry'
-require 'simplecov'
-SimpleCov.start
+#require 'simplecov'
+#SimpleCov.start
 require './lib/school'
 
 
@@ -16,7 +16,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 1' do
-    xit '1. School Instantiation' do
+    it '1. School Instantiation' do
       expect(School).to respond_to(:new).with(2).argument
       expect(@school1).to be_an_instance_of(School)
 
@@ -35,7 +35,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 2' do
-    xit '2. School #add_student_name' do
+    it '2. School #add_student_name' do
       expect(@school1).to respond_to(:add_student_name).with(1).argument
 
       @school1.add_student_name('Megan')
@@ -45,9 +45,31 @@ RSpec.describe 'School Spec Harness' do
       expect(@school1.student_names).to eq(['Megan', 'Aurora', 'Tim'])
     end
 
-    xit '3. School #end_time' do
+    it '3. School #end_time' do
       expect(@school1).to respond_to(:end_time).with(0).argument
       expect(@school1.end_time).to eq('16:00')
     end
   end
+
+  describe 'Iteration 3' do
+    it 'can tell if school is full time' do
+      expect(@school1.is_full_time?).to eq(true)
+    end
+
+    it 'can capitalize student names in array' do
+      @school1.add_student_name('Megan')
+      @school1.add_student_name('Aurora')
+      @school1.add_student_name('Tim')
+
+      expect(@school1.standard_student_names).to eq(["Megan", "Aurora", "Tim"])
+    end
+
+  end
+
+  describe 'Iteration 4' do
+    it 'can convert entime to 12 hour format' do
+      expect(@school.convert_end_time_to_clock_time).to eq("4:00")
+    end
+  end
+
 end

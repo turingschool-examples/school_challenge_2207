77c31ed9eeca524a886afc0efa455c258917f98f
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-07 11:28:28 -0600

    Add a functioning end_time method that passes a the given test

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 class School
 attr_reader :start_time, :hours_in_school_day
-attr_accessor :student_names, :end_as_float
+attr_accessor :student_names, :end_as_int
 
 def initialize(start_time, hours_in_school_day)
   @start_time = start_time
   @hours_in_school_day = hours_in_school_day
   @student_names = []
-  @end_as_float = 0
+  @end_as_int = 0
 end
 
 def add_student_name(name)
@@ -14,13 +14,14 @@ def add_student_name(name)
 end
 
 def end_time
-  @end_as_float = start_time.to_f + @hours_in_school_day
-  end_as_float.to_s
+  @end_as_int = start_time.to_i + @hours_in_school_day
+  end_as_int.to_s
+  "#{end_as_int}:00"
 end
 
 def is_full_time?
   self.end_time
-  if end_as_float > 4
+  if end_as_int > 4
     true
   else
     false

39bfe8fb1207078c7ae4d19b172ec2f3ac229e79
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-07 11:53:19 -0600

    Initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
+class School
+    attr_reader :start_time, :hours_in_school_day, :student_names
 
+    def initialize(start_time, hours_in_school_day)
+        @start_time = start_time
+        @hours_in_school_day = hours_in_school_day
+        @student_names = []
+    end
+
+    def add_student_name(student)
+        @student_names << student
+    end
+
+    def time_int
+        @start_time.to_i + @hours_in_school_day
+    end
+
+    def time_str
+        "#{time_int}:00"
+    end
+
+    def end_time
+        time_int
+        time_str
+    end
+
+    def is_full_time?
+        return true if hours_in_school_day > 4
+    end
+
+    def standard_student_names
+        capital_names = []
+
+        @student_names.each do |student|
+            capital_names << student.capitalize
+        end
+        
+        return capital_names
+    end
+end

b68066c13aafea84ed312f37b369b58bf69997ff
Kyongnam Kang <kangkna@gmail.com>
2022-07-07 12:49:03 -0400

    Initial commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,8 @@
-
+class School
+    attr_reader :hours_in_school_day, :start_time, :student_names
+    def initialize(start_time, hours_in_school_day)
+        @hours_in_school_day = hours_in_school_day
+        @start_time = start_time
+        @student_names = student_names
+    end
+end

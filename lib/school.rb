9732e440eb51e4c66f3b0efab8ae5a6576ea5cfb
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-07 11:47:53 -0600

    Initial Commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 
+class School 
+  attr_reader :start_time, :hours_in_school_day, :student_names, :end_time  
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+    @end_time = "16:00"
+  end
+
+  def add_student_name(name)
+    @student_names << name 
+  end
+
+  # def end_time
+  #   "16:00"
+  # end 
+
+end

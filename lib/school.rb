5b714c904f5a946d8d59760c865b07ac4db33050
Chris Lyman <christopher.lyman0@gmail.com>
2022-07-07 13:29:48 -0400

    finised iteration 4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
+class School
+attr_accessor :hours_in_school_day,
+              :start_time,
+              :student_names,
+              :standard_student_names,
+              :final_end_time
 
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+    @standard_student_names = []
+  end
+
+  def add_student_name(student_name)
+    student_names << student_name
+    standard_student_names << student_name.capitalize
+  end
+
+  def end_time
+    @final_end_time = start_time.to_i + hours_in_school_day
+    "#{final_end_time}:00"
+  end
+
+  def is_full_time?
+    if hours_in_school_day > 4
+      true
+    else
+      false
+    end
+  end
+
+  def convert_end_time_to_clock_time
+  end_time
+    if final_end_time > 12
+      converted_end_time = @final_end_time - 12
+    else
+      converted_end_time = @final_end_time
+    end
+
+    "#{converted_end_time}:00"
+  end
+
+end
--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 1' do
-    xit '1. School Instantiation' do
+    it '1. School Instantiation' do
       expect(School).to respond_to(:new).with(2).argument
       expect(@school1).to be_an_instance_of(School)
 
@@ -35,7 +35,7 @@ RSpec.describe 'School Spec Harness' do
   end
 
   describe 'Iteration 2' do
-    xit '2. School #add_student_name' do
+    it '2. School #add_student_name' do
       expect(@school1).to respond_to(:add_student_name).with(1).argument
 
       @school1.add_student_name('Megan')
@@ -45,9 +45,32 @@ RSpec.describe 'School Spec Harness' do
       expect(@school1.student_names).to eq(['Megan', 'Aurora', 'Tim'])
     end
 
-    xit '3. School #end_time' do
+    it '3. School #end_time' do
       expect(@school1).to respond_to(:end_time).with(0).argument
       expect(@school1.end_time).to eq('16:00')
+    end
+  end
+
+  describe '#is full time?' do
+    it 'determines if the school is full time' do
+      expect(@school1.is_full_time?).to be true
+    end
+  end
+
+  describe '@standard student names' do
+    it 'has an array of student names capitialized' do
+      @school1.add_student_name('Aurora')
+      @school1.add_student_name('tim')
+      @school1.add_student_name('megan')
+
+      expect(@school1.standard_student_names).to eq(["Aurora", "Tim", "Megan"])
+    end
+  end
+
+  describe '#convert_end_time_to_clock_time' do
+    it 'converts end time to clock time' do
+
+      expect(@school1.convert_end_time_to_clock_time).to eq("4:00")
     end
   end
 end

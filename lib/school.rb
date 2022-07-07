eb20278f19c517baf5be4c219b0fab056aeb9128
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-07 13:16:19 -0600

    refactor: repositioning attr_reader

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,7 @@
 class School
-    attr_reader :hours_in_school_day, :start_time, :student_names
+    attr_reader :hours_in_school_day, 
+                :start_time, 
+                :student_names
     def initialize(start_time, hours_in_school)
         @hours_in_school_day = hours_in_school
         @start_time = start_time

b5e693c678c6afd213f11119dd11a2ca5ff6a8e3
Astrid Hecht <silentaviary@gmail.com>
2022-07-07 11:04:51 -0600

    Write end_time method to pass iteration 2 part 2

--- lib/school.rb
+++ lib/school.rb
@@ -2,15 +2,23 @@ class School
     attr_reader :hours_in_school_day
     attr_reader :start_time
     attr_reader :student_names
+    
 
 
     def initialize(start, length, names = [])
         @hours_in_school_day = length
         @start_time = start 
         @student_names = names
+        @end_time = nil
     end
 
     def add_student_name(name)
         @student_names << name
     end
+
+    def end_time
+        @end_time = ((@start_time.tr(':', '').to_i) + (@hours_in_school_day * 100)).to_s.insert(-3,':')
+        return @end_time
+    end
+
 end

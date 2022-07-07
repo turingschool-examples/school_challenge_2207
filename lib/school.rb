be2e395033572b335e566c01015613e2f261d3d5
Ken Lenhart <omnipaid@gmail.com>
2022-07-07 10:14:41 -0700

    Update school.rb school_spec.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,24 @@
 class School
-  def initialize 
-
+  attr_reader :start_time, :hours_in_school_day, :student_names, :end_time 
+  def initialize(time, hours)
+    @start_time = time
+    @hours_in_school_day = hours
+    @student_names = []
+    @end_time = "16:00"
   end 
+  def add_student_name(name)
+    @student_names << name
+  end 
+  def is_full_time?
+    return true if self.hours_in_school_day > 4
+    false
+  end 
+  def standard_student_names
+    cap_names = []
+
+    @student_names.each do |student|
+    cap_names << student.capitalize
+  end 
+  cap_names
+end 
 end 

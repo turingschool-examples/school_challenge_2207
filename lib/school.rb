ebb3c120f7898f440a8327458a4dbbc61e7eda39
Astrid Hecht <silentaviary@gmail.com>
2022-07-07 10:48:22 -0600

    Write School class to complete iteration 1 tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
+class School
+    attr_reader :hours_in_school_day
+    attr_reader :start_time
+    attr_reader :student_names
 
+
+    def initialize(start, length, names = [])
+        @hours_in_school_day = length
+        @start_time = start 
+        @student_names = names
+    end
+end

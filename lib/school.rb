272919db7793e75e2d4d7ecee74ee71dca44767e
Shawn Lee <Shawnl93@hotmail.com>
2022-07-07 11:04:07 -0600

    Finished iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,9 @@
 class School
-  def initialize
+  attr_reader :start_time, :hours_in_school_day, :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
   end
-end    
+end
